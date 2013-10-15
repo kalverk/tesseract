cafa4acaefec0925334138c247b9f0efcc29793c
Kadi <kadi.laidoja@gmail.com>
2013-10-15 08:50:03 +0300

    ?

--- POS/src/ee/ut/math/tvt/tesseract/IntroUI.java
+++ POS/src/ee/ut/math/tvt/tesseract/IntroUI.java
@@ -2,9 +2,6 @@ package ee.ut.math.tvt.tesseract;
 
 import java.awt.Container;
 import java.awt.GridLayout;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Properties;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -14,51 +11,40 @@ import javax.swing.JLabel;
 public class IntroUI {
 
 	public static void run() {
-		Properties prop = new Properties();
-		Properties ver = new Properties();
-
-		try {
-			prop.load(new FileInputStream("application.properties"));
-			ver.load(new FileInputStream("version.properties"));
-
-		} catch (IOException ex) {
-			ex.printStackTrace();
-		}
+		// TODO Auto-generated method stub
 
 		JFrame raam = new JFrame("Intro");
-		raam.setSize(500, 300);
+		raam.setSize(500, 250);
 		raam.setLocation(100, 100);
 		Container sisu = raam.getContentPane();
-		sisu.setLayout(new GridLayout(0, 1));
+		sisu.setLayout(new GridLayout(0,1));
 		JButton nupp = new JButton();
-
+		
 		ImageIcon icon = new ImageIcon("logo.PNG");
-		JLabel t5 = new JLabel();
-		t5.setIcon(icon);
-		sisu.add(t5);
-
 		JLabel t1 = new JLabel();
-		t1.setText("Team name: " + prop.getProperty("teamname"));
+		t1.setText("Team name: TESSERACT");
 		sisu.add(t1);
+		
 
 		JLabel t2 = new JLabel();
-		t2.setText("Team leader: " + prop.getProperty("teamleader"));
+		t2.setText("Team leader: Kälver Kilvits");
 		sisu.add(t2);
-
+		
 		JLabel t3 = new JLabel();
-		t3.setText("Team leader's email: " + prop.getProperty("leadermail"));
+		t3.setText("Team leader's email: kalverk@gmail.com");
 		sisu.add(t3);
-
+		
 		JLabel t4 = new JLabel();
-		t4.setText("Team members: " + prop.getProperty("member1") + ", "
-				+ prop.getProperty("member2") + ", "
-				+ prop.getProperty("member3"));
+		t4.setText("Team members: Kälver Kilvits, Karin Klooster, Markus Kängsepp, Kadi Laidoja");
 		sisu.add(t4);
+		
 
+		JLabel t5 = new JLabel();
+		t5.setText("Team logo: ");
+		sisu.add(t5);
+		
 		JLabel t6 = new JLabel();
-		t6.setText("Version number: " + ver.getProperty("build.major.number")
-				+ "." + ver.getProperty("build.minor.number") + "."
-				+ ver.getProperty("build.revision.number"));
+		t6.setText("Version number: 0.0.0");
 		sisu.add(t6);
 
 		raam.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
