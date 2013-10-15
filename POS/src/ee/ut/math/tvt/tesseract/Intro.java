560fced166c17e10b98176d3cb8fe7903387db1a
Kadi <kadi.laidoja@gmail.com>
2013-10-15 15:58:45 +0300

    lisatud log- algne variant

--- POS/src/ee/ut/math/tvt/tesseract/Intro.java
+++ POS/src/ee/ut/math/tvt/tesseract/Intro.java
@@ -1,8 +1,25 @@
 package ee.ut.math.tvt.tesseract;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 
 public class Intro {
+	private static final Logger log = Logger.getLogger(Intro.class);
 	public static void main(String[] args) {
+		BasicConfigurator.configure();
+
+	
 		IntroUI.run();
+		
+		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		Date date = new Date();
+
+		log.info("Intro window is opened  "+dateFormat.format(date));
+
 	}
+	
 
 }
