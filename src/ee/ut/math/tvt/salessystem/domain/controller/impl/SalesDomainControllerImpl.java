c688b76ab240c50f2c6de813d404638620eb76f4
Karin Klooster <karinklooster@gmail.com>
2013-11-08 15:18:49 +0200

    uuesti

--- src/ee/ut/math/tvt/salessystem/domain/controller/impl/SalesDomainControllerImpl.java
+++ src/ee/ut/math/tvt/salessystem/domain/controller/impl/SalesDomainControllerImpl.java
@@ -7,6 +7,7 @@ import ee.ut.math.tvt.salessystem.domain.exception.VerificationFailedException;
 import ee.ut.math.tvt.salessystem.domain.controller.SalesDomainController;
 import ee.ut.math.tvt.salessystem.domain.data.SoldItem;
 import ee.ut.math.tvt.salessystem.domain.data.StockItem;
+import ee.ut.math.tvt.salessystem.util.HibernateUtil;
 
 /**
  * Implementation of the sales domain controller.
@@ -45,7 +46,7 @@ public class SalesDomainControllerImpl implements SalesDomainController {
 
 	@Override
 	public void endSession() {
-	//	HibernateUtil.closeSession();
+	HibernateUtil.closeSession();
 		
 	}
 }
