65657868c5532c566c2927b89be5dde84ffdfd83
Kadi <kadi.laidoja@gmail.com>
2013-11-08 15:07:14 +0200

    Revert "build.xml parandus"
    This reverts commit d04ebea9b48e606627bf75045d62dbdb8739062c.

--- src/ee/ut/math/tvt/salessystem/domain/controller/impl/SalesDomainControllerImpl.java
+++ src/ee/ut/math/tvt/salessystem/domain/controller/impl/SalesDomainControllerImpl.java
@@ -2,11 +2,12 @@ package ee.ut.math.tvt.salessystem.domain.controller.impl;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import ee.ut.math.tvt.hibernate.HibernateUtil;
 import ee.ut.math.tvt.salessystem.domain.exception.VerificationFailedException;
 import ee.ut.math.tvt.salessystem.domain.controller.SalesDomainController;
 import ee.ut.math.tvt.salessystem.domain.data.SoldItem;
 import ee.ut.math.tvt.salessystem.domain.data.StockItem;
-import ee.ut.math.tvt.salessystem.util.HibernateUtil;
 
 /**
  * Implementation of the sales domain controller.
