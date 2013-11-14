ab067e62a5eef0699014fedd30fb94220993a45e
Kadi <kadi.laidoja@gmail.com>
2013-11-14 10:54:57 +0200

    paar muutujanime vahetust, Salvestab sooritatud ostude info databaasi, aga hetkel annab NullpointerExceptioni ning ei sule akent, kui ost sooritatud.

--- src/ee/ut/math/tvt/salessystem/domain/controller/impl/SalesDomainControllerImpl.java
+++ src/ee/ut/math/tvt/salessystem/domain/controller/impl/SalesDomainControllerImpl.java
@@ -4,12 +4,6 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-
-
-
-
-
-
 import org.apache.log4j.Logger;
 
 import ee.ut.math.tvt.salessystem.domain.exception.VerificationFailedException;
@@ -27,45 +21,46 @@ import ee.ut.math.tvt.salessystem.service.HibernateDataService;
 public class SalesDomainControllerImpl implements SalesDomainController {
 
 	private static final Logger log = Logger.getLogger(SalesSystemUI.class);
-    private List<AcceptOrder> purchaseList;
-    private List<StockItem> stockItems;
-    HibernateDataService service = new HibernateDataService();
-    
-    
-    public SalesDomainControllerImpl() {
-            stockItems = new ArrayList<StockItem>();
-            purchaseList = new ArrayList<AcceptOrder>();
-            stockItems.addAll(service.getStockItems());
-            purchaseList.addAll(service.getAcceptOrders());
-            for (AcceptOrder p : purchaseList) {
-                    float total = 0;
-                    for (SoldItem s : p.getSoldItems()) {
-                            total += s.getSum();
-                    }
-                    p.setTotal(total);
-            }
-    }
+	private List<AcceptOrder> acceptorderList;
+	private List<StockItem> stockItems;
+	HibernateDataService service = new HibernateDataService();
 
-    public void submitCurrentPurchase(List<SoldItem> goods)
-                    throws VerificationFailedException {
-            for (SoldItem item : goods) {
-                    for (StockItem stockitem : stockItems) {
-                            if (stockitem.getId() == item.getId()) {
-                                    stockitem.setQuantity(stockitem.getQuantity()
-                                                    - item.getQuantity());
-                            }
-                    }
-            }
-            AcceptOrder purchase = new AcceptOrder(new Date(), goods);
-            purchaseList.add(purchase);
-            for (StockItem s: stockItems) {
-                    service.update(s);
-            }
-            service.addAcceptOrder(purchase);
-            for (SoldItem s : purchase.getSoldItems()) {
-                    s.setAcceptorder(purchase);
-                    service.addSoldItem(s);}
-            }
+	public SalesDomainControllerImpl() {
+		stockItems = new ArrayList<StockItem>();
+		acceptorderList = new ArrayList<AcceptOrder>();
+		stockItems.addAll(service.getStockItems());
+		acceptorderList.addAll(service.getAcceptOrders());
+		for (AcceptOrder p : acceptorderList) {
+			float total = 0;
+			for (SoldItem s : p.getSoldItems()) {
+				total += s.getSum();
+			}
+			p.setTotal(total);
+		}
+	}
+
+	public void submitCurrentPurchase(List<SoldItem> goods)
+			throws VerificationFailedException {
+		for (SoldItem item : goods) {
+			for (StockItem stockitem : stockItems) {
+				if (stockitem.getId() == item.getId()) {
+					stockitem.setQuantity(stockitem.getQuantity()
+							- item.getQuantity());
+				}
+			}
+		}
+		AcceptOrder purchase = new AcceptOrder(new Date(), goods);
+		acceptorderList.add(purchase);
+		for (StockItem s : stockItems) {
+			service.update(s);
+		}
+		service.addAcceptOrder(purchase);
+		for (SoldItem s : purchase.getSoldItems()) {
+			s.setAcceptorder(purchase);
+			service.addSoldItem(s);
+		}
+	}
+
 	public void cancelCurrentPurchase() throws VerificationFailedException {
 		// XXX - Cancel current purchase
 	}
@@ -99,7 +94,7 @@ public class SalesDomainControllerImpl implements SalesDomainController {
 	@Override
 	public void addStockItem(StockItem stockitem) {
 		service.addStockItem(stockitem);
-		
+
 	}
 
 	@Override
