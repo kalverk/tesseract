e275b37dcfe703892b4f25284e1f4e7af5a78a24
kalverk <kalverk@hotmail.com>
2013-11-19 10:59:03 +0200

    Tegin kaks lihtsamat automaatset testi ära. Õhtul võin teised kaks ka teha hetkel ei jõudnud.
    Test ise ei kompileeru, sest build on vigane, lisasin paar asja ja läks
    nagu paremaks aga päris õige ikkagi ei ole.
    
    Pushisin poolikuna ära, et keegi kahekordselt tööd ei teeks :)

---
+++ test/ee/ut/math/tvt/StockItemTest.java
@@ -0,0 +1,35 @@
+package ee.ut.math.tvt;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import ee.ut.math.tvt.salessystem.domain.data.StockItem;
+
+public class StockItemTest {
+	private StockItem item;
+	
+	@Before
+	public void setUp(){
+		item = new StockItem("Mingitoode", "mingikirjeldus", 12.00,99);
+	}
+	
+	@Test
+	public void testClone(){
+		StockItem clone = (StockItem)item.clone();
+		assertEquals(item.getId(),clone.getId());
+		assertEquals(item.getName(),clone.getName());
+		assertEquals(item.getDescription(),clone.getDescription());
+		assertEquals(item.getPrice(),clone.getPrice(), 0.01);
+		assertEquals(item.getQuantity(),clone.getQuantity());
+	}
+	
+	@Test
+	public void testGetColumn(){
+		assertEquals(item.getId(), item.getColumn(0));
+		assertEquals(item.getName(), item.getColumn(1));
+		assertEquals(item.getPrice(), item.getColumn(2));
+		assertEquals(item.getQuantity(), item.getColumn(3));
+	}
+}
